4d95eaf44a106e6f062eb4102c05964f18392345
Peihong Guo <peihongguo@gmail.com>
2017-04-06 01:11:55 -0500

    admin

--- matlab/FeaturePointsEval/driver.m
+++ matlab/FeaturePointsEval/driver.m
@@ -3,8 +3,8 @@ params.nbins = 8;
 params.cell_size = 8;
 params.nblocks = 4;
 
-img_path = '/Users/peihongguo/Codes/FaceSeg/InternetRecon3/';
-person = 'George_W_Bush';
+img_path = '/home/phg/Storage/FastStorage/Data/InternetRecon3/';
+person = 'Andy_Lau';
 
 image_files = dir(fullfile(img_path, person, '*.jpg'));
 N = length(image_files);
@@ -34,12 +34,18 @@ for i=1:N
     for j=1:Nfp
         x = pts{i}(1,j);
         y = pts{i}(2,j);
-        p = extract_patch(I{i}, x, y, params.feat_window_size);
-        
-        %figure(2);
-        %imshow(p);pause;
-        
-        fvectors(i, (j-1)*128+1:j*128) = HoG(p, [params.nbins, params.cell_size, params.nblocks, 1, 0.2])';
+
+        if false
+            p = extract_patch(I{i}, x, y, params.feat_window_size);
+
+            %figure(2);
+            %imshow(p);pause;
+
+            fvectors(i, (j-1)*128+1:j*128) = HoG(p, [params.nbins, params.cell_size, params.nblocks, 1, 0.2])';
+        else
+            %size(extractHOGFeatures(I{i}, [x, y], 'BlockSize', [4 4], 'NumBins', 8))
+            fvectors(i, (j-1)*128+1:j*128) = extractHOGFeatures(I{i}, [x, y], 'BlockSize', [4 4], 'NumBins', 8);
+        end
     end
 end
 
@@ -51,35 +57,44 @@ end
 end
 
 %% Build per-feature point model
+addpath ../inexact_alm_rpca;
+addpath ../inexact_alm_rpca/PROPACK;
 models = {};
 for j=1:Nfp
     fvectors_j = fvectors(:,(j-1)*128+1:j*128);
+    
+    % robust pca
+    %fvectors_j = inexact_alm_rpca(fvectors_j);
+    
     models{j}.mean = mean(fvectors_j);
     [coeff, score, latent, tsquared, explained] = pca(fvectors_j - repmat(models{j}.mean, N, 1));
     sum_explained = cumsum(explained);
-    idx = find(sum_explained>=25, 1);
+    idx = find(sum_explained>=50, 1);
     models{j}.pcs = coeff(:,1:idx);
 end
 
 %% Visualize recon
-total_score = [];
+total_score = zeros(N,1);
 for i=1:N    
     %figure(1);imshow(I{i});hold on;
-    total_score(i) = 0;
     for j=1:Nfp
         fvectors_ij = fvectors(i,(j-1)*128+1:j*128);
+        
         proj_ij = models{j}.pcs' * (fvectors_ij - models{j}.mean)';
         recon_ij = models{j}.pcs * reshape(proj_ij, [], 1) + models{j}.mean';
         score = norm(recon_ij - fvectors_ij');
+        
+        %score = norm(fvectors_ij - models{j}.mean);
+        
         total_score(i) = total_score(i) + score;
         %plot(pts{i}(1,j), pts{i}(2,j), '.', 'color', [score, 0.5, 1-score], 'markersize', 15);
-        %title(num2str(total_score));
+        %title(num2str(total_score(i)));
     end
     %pause;
 end
 
 %% Show the ordering
-[sorted_score, order] = sort(total_score, 2, 'descend');
+[sorted_score, order] = sort(total_score, 'descend');
 for i=1:N
-    figure(1);imshow(I{order(i)});title(num2str(sorted_score(i)));draw_shape(pts{order(i)}, 'g.');pause;
+    figure(1);imshow(I{order(i)});hold on;title(num2str(sorted_score(i)));draw_shape(pts{order(i)}, 'g.');pause;
 end
